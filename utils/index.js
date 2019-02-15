7e851cf9d3ff341a309c01d060e2553ec124bc54
yevhenii <yevgeniy.ovcharenkohome@gmail.com>
2019-02-15 18:20:06 +0200

    added filterer exports to index js file

--- utils/index.js
+++ utils/index.js
@@ -2,10 +2,12 @@ const arrays = require('./arrays');
 const zipper = require('./zipper');
 const differencer = require('./differencer');
 const droper = require('./droper');
+const filterer = require('./filterer');
 
 module.exports = {
   arrays,
   zipper,
   differencer,
   droper,
+  filterer,
 };
