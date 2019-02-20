7c67b6f3baeed8e869f3a048c102273092ce89dd
yevhenii <yevgeniy.ovcharenkohome@gmail.com>
2019-02-20 17:41:26 +0200

    fixed reading and writting data to file. need to add files validation

--- homework2/contents/index.js
+++ homework2/contents/index.js
@@ -1,61 +1,25 @@
 const fs = require('fs');
 
-function grab(flag) {
-  const index = process.argv.indexOf(flag);
-  return index === -1 ? null : process.argv[index + 1];
-}
+const PATH_CL = process.argv[2];
 
-function readFiles(dirname, onFileContent, onError) {
-  fs.readdir(dirname, (err, filenames) => {
-    if (err) {
-      onError(err);
+function readFiles(pathCl) {
+  fs.readdir(pathCl, (e, filenames) => {
+    if (e) {
+      console.log(e.message);
       return;
     }
     filenames.forEach(filename => {
-      fs.readFile(dirname + filename, 'utf-8', (error, content) => {
-        if (error) {
-          onError(error);
-          return;
+      fs.readFile(pathCl + filename, 'utf-8', (err, content) => {
+        if (err) {
+          console.log(err.message);
         }
-        onFileContent(filename, content);
+        fs.appendFile(`${PATH_CL}/contents.txt`, `${content}\n`, error => {
+          if (error) throw error;
+          console.log('Saved!');
+        });
       });
     });
   });
 }
 
-async function start() {
-  const data = {};
-
-  await readFiles(
-    grab('--path'),
-    (filename, content) => {
-      data[filename] = content;
-    },
-    err => {
-      throw err;
-    },
-  );
-
-  await fs.writeFile(
-    `/home/dev41/Documents/MOC/Practice/ma-nodejs-course-2019/homework2/contents/content.txt`,
-    data,
-    err => {
-      if (err) {
-        console.log(err.message);
-        return false;
-      }
-      console.log('file created. data inserted');
-    },
-  );
-}
-
-start();
-
-//   for (let a = 0, len = buf.length; a < len; a++) {
-//     if (buf[i] > 127) {
-//       break;
-//     } else {
-//       return files[i];
-//     }
-//   }
-// }
+readFiles(PATH_CL);
