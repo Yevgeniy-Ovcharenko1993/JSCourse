5c47b2689cd6b81cd2970ad25211cfe85c33ae1a
yevhenii <yevgeniy.ovcharenkohome@gmail.com>
2019-02-24 20:39:40 +0200

    implemented post request and dinamically changing maxTime

--- homework3/ping-pong-server/src/server.js
+++ homework3/ping-pong-server/src/server.js
@@ -2,38 +2,48 @@ const http = require('http');
 const url = require('url');
 
 const port = process.env.PORT || 8080;
-const timerResponse = parseInt(process.env.TIMER, 10) || 10000;
+let maxTime = 10000;
 
 const server = http.createServer((req, res) => {
-  const URL = req.url;
+  const URL = url.parse(req.url, true);
+  if (URL.pathname !== '/api/ping') {
+    res.end(JSON.stringify({ msg: 'Bad URL' }));
+  }
 
   let response = '';
 
   const { timer } = url.parse(URL, true).query;
+  console.log(timer);
   const intTime = parseInt(timer, 10);
 
-  if (req.method.toString() !== 'GET' || !URL.toString().includes('/api/ping')) {
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+      maxTime = JSON.parse(body);
+    });
+    req.on('end', () => {
+      console.log(parseInt(maxTime.timer, 10));
+      res.end(JSON.stringify(maxTime));
+    });
+  } else if (req.method.toString() !== 'GET') {
     response = JSON.stringify({ msg: 'Bad request' });
     res.setHeader('Content-type', 'application/json');
     res.statusCode = 400;
     res.end(response);
-  } else if (timerResponse < intTime) {
-    response = JSON.stringify({ msg: 'Your time is bigger than max time which is 10000 ms' });
-    res.setTimeout(timerResponse, () => {
+  } else if (parseInt(maxTime.timer, 10) < intTime) {
+    response = JSON.stringify({ msg: `Your time is bigger than max time which is ${maxTime} ms` });
+    const timerMAX = parseInt(maxTime.timer, 10);
+    res.setTimeout(timerMAX, () => {
       res.setHeader('Content-type', 'application/json');
       res.statusCode = 400;
       res.end(response);
     });
-  } else if (intTime.toString().length === 0) {
-    response = JSON.stringify({ msg: 'Pong' });
-    res.setHeader('Content-type', 'application/json');
-    res.statusCode = 200;
-    res.end(response);
   } else {
     response = JSON.stringify({ msg: 'Pong' });
-    res.setTimeout(timerResponse, () => {
+    res.setTimeout(intTime, () => {
       res.setHeader('Content-type', 'application/json');
-      res.statusCode = 400;
+      res.statusCode = 200;
       res.end(response);
     });
   }
