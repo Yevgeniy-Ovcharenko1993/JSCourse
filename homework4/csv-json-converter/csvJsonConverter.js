0d1dbb792eaef4aea93eb1a587118b12899d7ca5
yevhenii <yevgeniy.ovcharenkohome@gmail.com>
2019-03-07 00:36:36 +0200

    in progress

--- homework4/csv-json-converter/csvJsonConverter.js
+++ homework4/csv-json-converter/csvJsonConverter.js
@@ -2,44 +2,39 @@ const fs = require('fs');
 
 const path = require('path');
 
-const readFile = path.resolve(
-  __dirname,
-  '../csv-json-converter/files/annual-enterprise-survey-2017-financial-year-provisional.csv',
-);
+const readFile = path.resolve(__dirname, '../csv-json-converter/files/Small.csv');
 const writeFile = path.resolve(__dirname, '../csv-json-converter/files/result.json');
 
-// const PATH_CL = process.argv[2];
-
-function cSVFileConverter(pathParam, callback) {
-  const stream = fs.createReadStream(pathParam);
-  const data = [];
-  stream.on('readable', () => {
-    const chunk = stream.read();
-    if (chunk) {
-      const lines = chunk.toString().split('\n');
-      const headers = lines[0].split(',');
+function cSVFileConverter() {
+  const readStream = fs.createReadStream(readFile);
+  const writeStream = fs.createWriteStream(writeFile);
+  let headers = '';
+  let lines = '';
+  readStream.on('data', chunk => {
+    console.log('New Chunk received');
+    lines = chunk.toString().split('\n');
+    if (headers.length > 0) {
       for (let i = 1; i < lines.length; i += 1) {
         const obj = {};
         const currentLine = lines[i].split(',');
         for (let j = 0; j < headers.length; j += 1) {
           obj[headers[j]] = currentLine[j];
         }
-        data.push(obj);
+        writeStream.write(JSON.stringify([obj]));
+      }
+    } else {
+      headers = lines[0].split(',');
+      lines = chunk.toString().split('\n');
+      for (let i = 1; i < lines.length; i += 1) {
+        const obj = {};
+        const currentLine = lines[i].split(',');
+        for (let j = 0; j < headers.length; j += 1) {
+          obj[headers[j]] = currentLine[j];
+        }
+        writeStream.write(JSON.stringify([obj]));
       }
     }
   });
-
-  stream.on('close', () => {
-    callback(null, data);
-  });
-
-  stream.on('error', err => {
-    callback(err);
-  });
 }
 
-cSVFileConverter(readFile, (err, data) => {
-  console.log(err);
-  const stream = fs.createWriteStream(writeFile);
-  stream.write(JSON.stringify(data));
-});
+cSVFileConverter();
