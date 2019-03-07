27c40554e205933639b4f20e35ce3f984793b95a
yevhenii <yevgeniy.ovcharenkohome@gmail.com>
2019-03-07 22:41:14 +0200

    optimized algoritm

--- homework4/csv-json-converter/csvJsonConverter.js
+++ homework4/csv-json-converter/csvJsonConverter.js
@@ -2,42 +2,39 @@ const fs = require('fs');
 
 const path = require('path');
 
-const readFile = path.resolve(__dirname, '../csv-json-converter/files/small.csv');
+const readFile = path.resolve(__dirname, '../csv-json-converter/files/Small.csv');
 const writeFile = path.resolve(__dirname, '../csv-json-converter/files/result.json');
 
-function cSVFileConverter() {
+function converter(headers, lines) {
+  const data = [];
+  for (let i = 1; i < lines.length; i += 1) {
+    const obj = {};
+    const currentLine = lines[i].split(',');
+    for (let j = 0; j < headers.length; j += 1) {
+      obj[headers[j]] = currentLine[j];
+    }
+    data.push(obj);
+  }
+  return data;
+}
+
+function applyConvertedData() {
   const readStream = fs.createReadStream(readFile);
   const writeStream = fs.createWriteStream(writeFile);
+
   let headers = '';
   let lines = '';
-  const data = [];
+
   readStream.on('data', chunk => {
     console.log('New Chunk received');
     lines = chunk.toString().split('\n');
+    headers = lines[0].split(',');
     if (headers.length > 0) {
-      for (let i = 1; i < lines.length; i += 1) {
-        const obj = {};
-        const currentLine = lines[i].split(',');
-        for (let j = 0; j < headers.length; j += 1) {
-          obj[headers[j]] = currentLine[j];
-        }
-        data.push(obj);
-      }
-      writeStream.write(JSON.stringify(data));
+      writeStream.write(JSON.stringify(converter(headers, lines)));
     } else {
-      headers = lines[0].split(',');
-      lines = chunk.toString().split('\n');
-      for (let i = 1; i < lines.length; i += 1) {
-        const obj = {};
-        const currentLine = lines[i].split(',');
-        for (let j = 0; j < headers.length; j += 1) {
-          obj[headers[j]] = currentLine[j];
-        }
-        data.push(obj);
-      }
-      writeStream.write(JSON.stringify(data));
+      writeStream.write(JSON.stringify(converter(headers, lines)));
     }
   });
 }
 
-cSVFileConverter();
+applyConvertedData();
