5cc873e637d0a2b83e930cb99a873abc02d6fd94
yevhenii <yevgeniy.ovcharenkohome@gmail.com>
2019-03-12 00:40:17 +0200

    implemented convertation

--- homework4/csv-json-converter/csvJsonConverter.js
+++ homework4/csv-json-converter/csvJsonConverter.js
@@ -1,39 +1,50 @@
+const path = require('path');
+const readline = require('readline');
 const fs = require('fs');
 
-const path = require('path');
-
-const readFile = path.resolve(__dirname, '../csv-json-converter/files/Small.csv');
+const readFile = path.resolve(
+  __dirname,
+  '../csv-json-converter/files/annual-enterprise-survey-2017-financial-year-provisional.csv',
+);
 const writeFile = path.resolve(__dirname, '../csv-json-converter/files/result.json');
 
-function converter(headers, lines) {
-  const data = [];
-  for (let i = 1; i < lines.length; i += 1) {
+const rl = readline.createInterface({
+  input: fs.createReadStream(readFile),
+});
+
+function converter(headers, line) {
+  let data = '';
+  for (let i = 1; i < line.length; i += 1) {
     const obj = {};
-    const currentLine = lines[i].split(',');
     for (let j = 0; j < headers.length; j += 1) {
-      obj[headers[j]] = currentLine[j];
+      obj[headers[j]] = line[j];
     }
-    data.push(obj);
+    data = obj;
   }
   return data;
 }
 
 function applyConvertedData() {
-  const readStream = fs.createReadStream(readFile);
   const writeStream = fs.createWriteStream(writeFile);
 
-  let headers = '';
-  let lines = '';
-
-  readStream.on('data', chunk => {
-    console.log('New Chunk received');
-    lines = chunk.toString().split('\n');
-    headers = lines[0].split(',');
-    if (headers.length > 0) {
-      writeStream.write(JSON.stringify(converter(headers, lines)));
-    } else {
-      writeStream.write(JSON.stringify(converter(headers, lines)));
+  let headers;
+  let lines;
+  writeStream.write('[');
+  rl.on('line', line => {
+    if (!headers) {
+      headers = line.toString().split(',');
+      return;
     }
+    if (!lines) {
+      lines = line.split(',');
+      writeStream.write(JSON.stringify(converter(headers, line.split(','))));
+    } else {
+      writeStream.write(`, ${JSON.stringify(converter(headers, line.split(',')))}`);
+    }
+  });
+
+  rl.on('close', line => {
+    writeStream.write(']');
   });
 }
 
